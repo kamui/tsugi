8bb232aa12e32e0172d95bac6202224b49c83e9e
Jack Chu <jack@jackchu.com>
2019-02-11 23:44:31 -0500

    convert jest config to ts

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,12 @@
 module.exports = {
-  "moduleNameMapper": {
+  moduleNameMapper: {
     ".+\\.(css|pcss)$": "identity-obj-proxy",
-    ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/tests/mocks/file_mock.js",
-    "^tsugi/(.+)": "<rootDir>$1"
+    ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/tests/mocks/file_mock.js",
+    "^tsugi/(.+)": "<rootDir>$1",
   },
-  "setupFiles": [
-    "raf/polyfill",
-    "jest-localstorage-mock"
-  ],
-  "setupTestFrameworkScriptFile": "./tests/setup_test_framework_script_file.js",
-  "testURL": "http://localhost",
-  "testPathIgnorePatterns": [
-    "/node_modules/",
-    "/cypress",
-  ],
+  setupFiles: ["raf/polyfill", "jest-localstorage-mock"],
+  setupFilesAfterEnv: ["./tests/setup_test_framework_script_file.ts"],
+  testURL: "http://localhost",
+  testPathIgnorePatterns: ["/node_modules/", "/cypress"],
 }
