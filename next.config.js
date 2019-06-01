31e776d3a4b5dae5c2412cd68d1d79a6100caae4
Jack Chu <jack@jackchu.com>
2019-06-01 17:22:18 -0400

    Add next-offline support

--- next.config.js
+++ next.config.js
@@ -3,41 +3,46 @@ const { assetPrefix } = require("./commonjs/config.ts")
 const { isProductionLike } = require("./commonjs/config.ts")
 const withTypescript = require("@zeit/next-typescript")
 const withCSS = require("@zeit/next-css")
+const withOffline = require("next-offline")
 const withBundleAnalyzer = require("@next/bundle-analyzer")
 // const fs = require("fs")
 
 module.exports = withTypescript(
   withBundleAnalyzer(
-    withCSS({
-      assetPrefix,
-      cssModules: true,
-      cssLoaderOptions: {
-        importLoaders: 1,
-        localIdentName: "[local]---[hash:base64:5]",
-      },
-      distDir: "build",
-
-      analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
-      analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
-      bundleAnalyzerConfig: {
-        server: {
-          analyzerMode: "static",
-          reportFilename: "../bundles/server.html",
+    withOffline(
+      withCSS({
+        assetPrefix,
+        cssModules: true,
+        cssLoaderOptions: {
+          importLoaders: 1,
+          localIdentName: "[local]---[hash:base64:5]",
         },
-        browser: {
-          analyzerMode: "static",
-          reportFilename: "../bundles/client.html",
-        },
-      },
+        distDir: "build",
 
-      webpack: (config, options) => {
-        config.mode = isProductionLike ? "production" : "development"
-        config.devtool = options.dev
-          ? "cheap-module-inline-source-map"
-          : "hidden-source-map"
-        config.plugins.push(new EnvironmentPlugin(process.env))
-        return config
-      },
-    })
+        analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
+        analyzeBrowser: ["browser", "both"].includes(
+          process.env.BUNDLE_ANALYZE
+        ),
+        bundleAnalyzerConfig: {
+          server: {
+            analyzerMode: "static",
+            reportFilename: "../bundles/server.html",
+          },
+          browser: {
+            analyzerMode: "static",
+            reportFilename: "../bundles/client.html",
+          },
+        },
+
+        webpack: (config, options) => {
+          config.mode = isProductionLike ? "production" : "development"
+          config.devtool = options.dev
+            ? "cheap-module-inline-source-map"
+            : "hidden-source-map"
+          config.plugins.push(new EnvironmentPlugin(process.env))
+          return config
+        },
+      })
+    )
   )
 )
