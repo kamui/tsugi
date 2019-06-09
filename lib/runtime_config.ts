456dbbd347a9fd92b884cb944c7dd4ad60e0a96b
Jack Chu <jack@jackchu.com>
2019-06-09 00:39:41 -0400

    convert runtime_config.ts to export in esnext format

--- lib/runtime_config.ts
+++ lib/runtime_config.ts
@@ -1,7 +1,9 @@
+export const publicRuntimeConfig = {
+  environment: process.env.APP_ENV,
+  apiUrl: process.env.BACKEND_API_URL,
+  assetPrefix: process.env.ASSET_HOST || "",
+}
+
 export default {
-  publicRuntimeConfig: {
-    environment: process.env.APP_ENV,
-    apiUrl: process.env.BACKEND_API_URL,
-    assetPrefix: process.env.ASSET_HOST || "",
-  },
+  publicRuntimeConfig,
 }
