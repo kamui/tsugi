9df3079ce0958cbbdba511ade09bda41cd9fe324
Jack Chu <jack@jackchu.com>
2019-07-01 17:13:35 -0400

    Don't match pcss extension in jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   moduleNameMapper: {
-    ".+\\.(css|pcss)$": "identity-obj-proxy",
+    ".+\\.(css)$": "identity-obj-proxy",
     ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/tests/mocks/file_mock.js",
     "^tsugi/(.+)": "<rootDir>$1",
